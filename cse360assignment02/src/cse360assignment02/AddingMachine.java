2ef39689c211cb0e32b2c77cbe2a89561e73279e
Samuel Trujillo <satrujil@asu.edu>
2020-10-05 15:52:37 -0700

    First commit

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,66 @@
+package cse360assignment02;
+/**
+* The AddingMachine program 
+* simply adds or subtracts an inputted number from a saved total
+* then is formatted into a string and the output is displayed on the screen.
+* @author  Samuel Trujillo
+* @since   2020-10-05
+*/
+public class AddingMachine {
+	  private int total;
+	  /**
+	   * This method is used to initialize int total
+	   * @return nothing
+	   */
+	  public AddingMachine () 
+	  {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * The getTotal method returns the current total
+	   * @return integer total
+	   */
+	  public int getTotal () 
+	  {
+	    return 0;
+	  }
+	 
+	  /**
+	   * The add method adds the parameter to the total variable
+	   * @param value, This is the first parameter to the add method
+	   * @return nothing
+	   */
+	  public void add (int value) 
+	  {
+		  
+	  }
+	  
+	   /**
+	   * The subtract method subtracts the parameter from the total variable
+	   * @param value, This is the first parameter to the subtract method
+	   * @return nothing
+	   */
+	  public void subtract (int value) 
+	  {
+		  
+	  }
+	 
+	  /**
+	   * This method calls methods add and subtract and formats the return into a String
+	   * @return String This returns 
+	   */
+	  public String toString () 
+	  {
+	    return "";
+	  }
+	  
+	  /**
+	   * This method is used to clear the memory
+	   * @return nothing
+	   */
+	  public void clear() 
+	  {
+		  
+	  }
+	}
